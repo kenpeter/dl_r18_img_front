9fb377908a6d01592aa981b4008247a786ac3c1b
kenpeter <figo2476@hotmail.com>
2017-03-18 23:51:59 +1100

    working

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="main">
+    <ul class="list-inline">
+      <li v-for="(item, index) in list" class="list-group-item">
+        <a :href="`http://dl_r18_img_back.local/upload/${item.category.name}/${item.fileName}`" target="_blank">
+          <img :src="`http://dl_r18_img_back.local/upload/${item.category.name}/${item.fileName}`"
+            class="img-thumbnail"
+            width="90"
+            height="122"
+          />
+        </a>
+      </li>
+    </ul>
 
     <VPaginator :options="options" :resource_url="resource_url" @update="updateResource"></VPaginator>
   </div>
@@ -10,22 +21,13 @@ export default {
   data() {
     return {
       list: [],
-      resource_url: 'http://dl_r18_img_back.local',
-      options: {
-        remote_data: 'nested.data',
-        remote_current_page: 'nested.current_page',
-        remote_last_page: 'nested.last_page',
-        remote_next_page_url: 'nested.next_page_url',
-        remote_prev_page_url: 'nested.prev_page_url',
-        next_button_text: 'Go Next',
-        previous_button_text: 'Go Back',
-      },
+      resource_url: 'http://dl_r18_img_back.local/?page=10&limit=100',
+      options: {},
     };
   },
   methods: {
     updateResource(data) {
       /* eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
-      console.log(data);
       this.list = data;
     },
   },
